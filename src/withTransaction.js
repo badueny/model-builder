7bfd805a4dfb1e10a48be4a41f327469722aac0b
Awenk <33746071+badueny@users.noreply.github.com>
2025-07-06 08:36:18 +0700

    add log withTransaction.js

--- src/withTransaction.js
+++ src/withTransaction.js
@@ -3,21 +3,25 @@ const makeModel = require('./Model');
 
 async function withTransaction(callback) {
   const conn = await db.getConnection();
+  console.log('🔌 [TX] Connection opened');
+
   try {
     await conn.beginTransaction();
+    console.log('🔐 [TX] Transaction started');
 
-    // Inject koneksi ke Model
     const Model = (table) => makeModel(table, conn);
-
     const result = await callback(conn, Model);
 
     await conn.commit();
+    console.log('✅ [TX] Committed');
     return result;
   } catch (err) {
     await conn.rollback();
+    console.error('❌ [TX] Rolled back:', err.message);
     throw err;
   } finally {
     conn.release();
+    console.log('🔚 [TX] Connection released');
   }
 }
 
